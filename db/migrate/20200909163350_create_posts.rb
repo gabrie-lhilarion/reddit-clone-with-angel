6c4dad4fa661492a1f6ed90d11ea5953da1804c7
AngelMoraDev <user@debian-10.development>
2020-09-09 12:19:56 -0500

    pending issue with foreign key disrupts migration

--- db/migrate/20200909163350_create_posts.rb
+++ db/migrate/20200909163350_create_posts.rb
@@ -1,8 +1,15 @@
 class CreatePosts < ActiveRecord::Migration[5.2]
   def change
     create_table :posts do |t|
+      t.string :title
+      t.string :content
+      t.string :tags
+      t.string :images
+      t.string :comments
+      t.references :user, foreign_key: true
 
       t.timestamps
     end
+
   end
 end
