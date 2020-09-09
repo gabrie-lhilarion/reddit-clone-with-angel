6f55526fefebaabf2da435b43d4dfd9ad52dbc2a
AngelMoraDev <user@debian-10.development>
2020-09-09 17:42:08 -0500

    completed association of posts with users

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,7 @@ class User < ApplicationRecord
     validates :user_name, presence: true
     validates :first_name, length: { maximum: 14 }
     validates :last_name, length: { minimum: 3 }
+
+    has_many :posts
+
 end
