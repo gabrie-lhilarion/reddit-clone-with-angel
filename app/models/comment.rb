a6ac5940e0dc0fc38df5e54d2a2b7e651671ece2
AngelMoraDev <angelmomaa@gmail.com>
2020-09-10 19:06:13 -0500

    debug app, ready to review

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,9 +1,8 @@
 class Comment < ApplicationRecord
-    belongs_to :user
-    belongs_to :post
+  belongs_to :user
+  belongs_to :post
 
-    validates :body, presence: true
-    validates :user, presence: true
-    validates :post, presence: true
-
+  validates :body, presence: true
+  validates :user, presence: true
+  validates :post, presence: true
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,4 @@
 class Post < ApplicationRecord
-    belongs_to :user 
-    has_many :comments, inverse_of: :post, dependent: :destroy
+  belongs_to :user
+  has_many :comments, inverse_of: :post, dependent: :destroy
 end
-#has_many :line_items, inverse_of: :
